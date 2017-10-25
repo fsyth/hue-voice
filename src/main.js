f51a34ae5df7b5968f13bef0f8537cb912d8e1d6
James Forsyth <forsythjames@hotmail.co.uk>
2017-10-25 20:54:38 +0100

    Tidied up and commented example.

--- src/main.js
+++ src/main.js
@@ -1,71 +1,8 @@
 /*global Hue, HueCommand*/
 
-
-// Debug message area
+// Debug message area with example callbacks to show that communication is
+// succesful or not
 const debugMessage = document.getElementById('debug')
-
-
-// Must provide your own Hue parameters here
-const hue = new Hue({ id: '4vy12cW4AIfRfeTgm8Gr6TBOhY1oVsNBoAW4aMnO' }, onSuccess, onError)
-let roomNo = '1'
-
-
-// Just use the preset commands, but you could add your own
-const hueCommands = HueCommand.presets
-
-
-// Allow Hue developer ID to be changed without going into the console
-const devIdInput = document.getElementById('devId')
-devIdInput.value = hue.id
-devIdInput.onchange = e => {
-  hue.reset()
-  hue.id = devIdInput.value
-  hue.setup(onSuccess, onError)
-}
-
-
-// Typing commands instead of speech
-const typingInput = document.getElementById('typing')
-typingInput.onchange = e => {
-  for (let command of hueCommands)
-    command.matchAndRun(typingInput.value, hue, roomNo, onSuccess, onError)
-
-  typingInput.value = ''
-}
-
-
-// SpeechRecognition API is new so has vendor prefixes currently and may not be supported
-const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
-
-if (!SpeechRecognition)
-  alert('Your browser does not support the Web Speech API. Please try again using an up to date version of Chrome.')
-
-const speechRec = new SpeechRecognition()
-speechRec.lang = navigator.language || 'en-US'
-speechRec.continuous = false
-speechRec.interimResults = false
-
-speechRec.onresult = e => {
-  let speech = e.results[0][0].transcript
-  console.log(speech)
-  document.getElementById('speech').innerText = speech
-
-  for (let command of hueCommands)
-    command.matchAndRun(speech, hue, roomNo, onSuccess, onError)
-}
-
-speechRec.onspeechend = () => {
-  speechRec.stop()
-}
-
-speechRec.onend = () => {
-  speechRec.start()
-}
-
-speechRec.start()
-
-
-// Example callbacks to show that communication is succesful or not
 
 function onSuccess(e) {
   fadeFrom('#080')
@@ -85,3 +22,75 @@ function fadeFrom(color) {
     debugMessage.style.color = 'transparent'
   }, 2e3)
 }
+
+
+
+// Hue class for interfacing with Philips Hue
+// Provide your own Hue parameters here, or use the devId input to set later
+const hue = new Hue({ id: '4vy12cW4AIfRfeTgm8Gr6TBOhY1oVsNBoAW4aMnO' }, onSuccess, onError)
+let roomNo = '1'
+
+// Allow Hue developer ID to be changed from a text input without needing to go
+// into the console
+const devIdInput = document.getElementById('devId')
+devIdInput.value = hue.id
+devIdInput.onchange = e => {
+  hue.reset()
+  hue.id = devIdInput.value
+  hue.setup(onSuccess, onError)
+}
+
+
+
+// HueCommand class for interpreting plain english commands as Hue states.
+// Using the preset commands here, but you could add your own.
+const hueCommands = HueCommand.presets
+
+// Allow commands to be typed instead of spoken if desired using a text input.
+const typingInput = document.getElementById('typing')
+typingInput.onchange = e => {
+  for (let command of hueCommands)
+    command.matchAndRun(typingInput.value, hue, roomNo, onSuccess, onError)
+
+  typingInput.value = ''
+}
+
+
+
+// Use the Web Speech API to get plain english commands from the user.
+// SpeechRecognition is new so has vendor prefixes currently and may not be supported
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+
+if (!SpeechRecognition)
+  alert('Your browser does not support the Web Speech API.\n' +
+        'Please try again using an up to date version of Chrome.')
+
+const speechRec = new SpeechRecognition()
+speechRec.lang = navigator.language || 'en-US'
+speechRec.continuous = false
+speechRec.interimResults = false
+
+speechRec.onresult = e => {
+  let speech = e.results[0][0].transcript
+
+  // Actually do something with the detected speech. In this case, display it
+  // and check it against the list of commands to control Hue.
+
+  document.getElementById('speech').innerText = speech
+
+  for (let command of hueCommands)
+    command.matchAndRun(speech, hue, roomNo, onSuccess, onError)
+}
+
+// Stop when speech ends to clear old transcript
+speechRec.onspeechend = () => {
+  speechRec.stop()
+}
+
+// Start again on ending for continuous speech recognition
+speechRec.onend = () => {
+  speechRec.start()
+}
+
+speechRec.start()
+
